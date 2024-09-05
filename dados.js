498df533aceb196aef5ee06f61eaf29dffc8d00d
junior <juniordefreitas1@gmail.com>
2024-09-05 07:29:00 -0300

    primeiro commit

---
+++ dados.js
@@ -0,0 +1,20 @@
+let listaDeVinhos = [
+    {
+        nome: "Cabernet Sauvignon",
+        tipo: "Tinto",
+        descricao: "Vinho tinto de corpo encorpado, com taninos presentes e aromas de frutas negras, especiarias e às vezes toques defumados.",
+        harmonizacao: "Carnes vermelhas grelhadas, massas com molhos robustos, queijos curados."
+    },
+    {
+        nome: "Chardonnay",
+        tipo: "Branco",
+        descricao: "Vinho branco versátil, podendo ser frutado, com notas de frutas tropicais e cítricos, ou mais complexo, com aromas de manteiga e baunilha.",
+        harmonizacao: "Peixes brancos, frutos do mar, aves, massas com molhos leves, queijos suaves."
+    },
+    {
+        nome: "Rosé de Provença",
+        tipo: "Rosé",
+        descricao: "Vinho rosé seco e elegante, com cor rosa pálida e aromas de frutas vermelhas, flores e toques cítricos.",
+        harmonizacao: "Saladas, carnes brancas, frutos do mar, queijos frescos."
+    }
+];
