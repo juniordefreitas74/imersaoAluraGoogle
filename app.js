62c0b2c8d8c6acebe9f609c341807176c2f12ab3
junior <juniordefreitas1@gmail.com>
2024-09-06 12:04:21 -0300

    novo

--- app.js
+++ app.js
@@ -1,1 +1,60 @@
-console.log(listaDeVinhos); 
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+    console.log(campoPesquisa);
+    
+    // Inicializa uma string vazia para armazenar os resultados da pesquisa
+    let resultados = "";
+    let tipo = "";
+    let nome = "";
+    let descricao = "";
+    let harmonizacao = "";
+
+
+    if(!campoPesquisa){
+        section.innerHTML = "<span style='color:white;'>Hummm, não encontrei nada com essa busca. <br>Que tal tentar com o nome de um vinho, <br>uma uva ou um prato específico? </span>";
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase() // transforma tudo que for digitado em minusculo
+
+    // Itera sobre cada vinho na lista de vinhos
+    for (let dado of listaDeVinhos) {
+        
+        tipo = dado.tipo.toLocaleLowerCase()
+        nome = dado.nome.toLocaleLowerCase()
+        descricao = dado.descricao.toLocaleLowerCase()
+        harmonizacao = dado.harmonizacao.toLocaleLowerCase()
+        
+        // se dado.nome.includes for igual ao campoPesquisa, então faça
+        if (nome.includes(campoPesquisa) || tipo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || harmonizacao.includes(campoPesquisa)){
+            // cria um novo elemento
+            resultados += `
+            <div class="item-resultado">
+                
+                <h2><a href="#" target="_blank"> ${dado.nome} </a></h2> 
+                
+                <p class="descricao-meta"><strong> Tipo: </strong> ${dado.tipo}</p>
+                <p class="descricao-meta"> <strong> Descrição: </strong> ${dado.descricao}</p>
+                <p class="descricao-meta"><strong> Harmonização: </strong> ${dado.harmonizacao}</p>
+                
+                <a href= ${dado.link} target="_blank"> <u><strong><i> Mais Informações </i></strong></a></u>
+            </div>
+        `;
+
+        }
+        console.log(dado.nome.includes(campoPesquisa))
+        // Constrói o HTML para cada item da pesquisa, formatando os dados do vinho
+        
+    }
+
+    if (!resultados){
+        resultados = "<span style='color:white;'>Nada foi encontrado!</span>"
+    }
+
+    // Atribui o HTML gerado para a seção de resultados
+    section.innerHTML = resultados;
+}
--- dados.js
+++ dados.js
@@ -1,20 +1,40 @@
 let listaDeVinhos = [
     {
-        nome: "Cabernet Sauvignon",
-        tipo: "Tinto",
-        descricao: "Vinho tinto de corpo encorpado, com taninos presentes e aromas de frutas negras, especiarias e às vezes toques defumados.",
-        harmonizacao: "Carnes vermelhas grelhadas, massas com molhos robustos, queijos curados."
+        nome: "Malbec",
+        tipo: "Tinto Seco",
+        descricao: "Vinho tinto de corpo médio a encorpado, com taninos macios e aromas de frutas vermelhas maduras, especiarias e chocolate.",
+        harmonizacao: "Carnes vermelhas grelhadas, massas com molhos robustos, queijos maturados.",
+        link: "https://pt.wikipedia.org/wiki/Malbec",
+        
     },
     {
         nome: "Chardonnay",
-        tipo: "Branco",
-        descricao: "Vinho branco versátil, podendo ser frutado, com notas de frutas tropicais e cítricos, ou mais complexo, com aromas de manteiga e baunilha.",
-        harmonizacao: "Peixes brancos, frutos do mar, aves, massas com molhos leves, queijos suaves."
+        tipo: "Branco Seco",
+        descricao: "Vinho branco versátil, com aromas de frutas cítricas, maçã verde e, em alguns casos, notas de baunilha e manteiga.",
+        harmonizacao: "Peixes brancos, frutos do mar, aves, massas leves e queijos suaves.",
+        link: "https://pt.wikipedia.org/wiki/Chardonnay",
     },
     {
-        nome: "Rosé de Provença",
-        tipo: "Rosé",
-        descricao: "Vinho rosé seco e elegante, com cor rosa pálida e aromas de frutas vermelhas, flores e toques cítricos.",
-        harmonizacao: "Saladas, carnes brancas, frutos do mar, queijos frescos."
+        nome: "Espumante ou Frisante",
+        tipo: "Espumante",
+        descricao: "Vinho espumante ou frisante, com bolhas finas e persistentes, aromas de frutas cítricas e maçã verde.",
+        harmonizacao: "Aperitivos, frutos do mar, sushi, carnes brancas e queijos leves.",
+        link: "https://pt.wikipedia.org/wiki/Vinho_espumante",
+    },
+
+    {
+        nome: "Carbenet",
+        tipo: "Espumante",
+        descricao: "Vinho espumante ou frisante, com bolhas finas e persistentes, aromas de frutas cítricas e maçã verde.",
+        harmonizacao: "Aperitivos, frutos do mar, sushi, carnes brancas e queijos leves.",
+        link: "https://pt.wikipedia.org/wiki/Vinho_espumante",
+    },
+
+    {
+        nome: "Carmenere",
+        tipo: "Espumante",
+        descricao: "Vinho espumante ou frisante, com bolhas finas e persistentes, aromas de frutas cítricas e maçã verde.",
+        harmonizacao: "Aperitivos, frutos do mar, sushi, carnes brancas e queijos leves.",
+        link: "https://pt.wikipedia.org/wiki/Vinho_espumante",
     }
 ];
